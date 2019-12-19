0b9b604568bf2a26973eb00e7773b5b15a238a6b
Andrew Qu <qua@rpi.edu>
2019-12-19 18:40:35 -0500

    initial square rendering

--- kakurasu/src/index.js
+++ kakurasu/src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Square represents each individual boardgame square
+// TODO
+// differentiate between clickable square and data square
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+ 
+      </button>
+    )
+  }
+}
+
+// Board represents the state of the board including the bordering
+// squares and the pieces that can be clicked
+// TODO
+// generates squares in a certain way
+class Board extends React.Component {
+
+  generateRow(dim) {
+
+    let a = [];
+    for (let i = 0; i < dim; i++) {
+      a.push((
+        <div className="board-row">
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+        </div>
+      ))
+    }
+    return (a);
+  }
+
+  render() {
+    return (
+    <div>
+      {this.generateRow(6)}
+    </div>
+    )
+  }
+}
+
+// Game is a wrapper around Board
+class Game extends React.Component {
+
+  render() {
+    return (
+      <Board/>
+    )
+  }
+}
+
+
+
+ReactDOM.render(<Game />, document.getElementById('root'))
